0e40d94447bfbe2708ef708cc6d8630261b912d5
Zhenyi He <snowymohehe@gmail.com>
2021-04-03 02:05:44 +0800

    :art: print one int after receiving data

--- TcpServer.js
+++ TcpServer.js
@@ -8,13 +8,13 @@ const port = 8080;
 const server = new Net.Server();
 // The server listens to a socket for a client to make a connection request.
 // Think of a socket as an end point.
-server.listen(port, function() {
+server.listen(port, function () {
     console.log(`Server listening for connection requests on socket localhost:${port}.`);
 });
 
 // When a client requests a connection with the server, the server creates a new
 // socket dedicated to that client.
-server.on('connection', function(socket) {
+server.on('connection', function (socket) {
     console.log('A new connection has been established.');
 
     // Now that a TCP connection has been established, the server can send data to
@@ -22,18 +22,18 @@ server.on('connection', function(socket) {
     socket.write('Hello, client.');
 
     // The server can also receive data from the client by reading from its socket.
-    socket.on('data', function(chunk) {
-        console.log(`Data received from client: ${chunk.toString()}.`);
+    socket.on('data', function (chunk) {
+        console.log(`Data received from client:`, "int", new Buffer.from(chunk).readInt32BE());
     });
 
     // When the client requests to end the TCP connection with the server, the server
     // ends the connection.
-    socket.on('end', function() {
+    socket.on('end', function () {
         console.log('Closing connection with the client');
     });
 
     // Don't forget to catch error, for your own sake.
-    socket.on('error', function(err) {
+    socket.on('error', function (err) {
         console.log(`Error: ${err}`);
     });
 });
